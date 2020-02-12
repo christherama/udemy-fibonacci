860ae45d2ac93444884629992bcd9f9372c3cf1a
Chris Ramacciotti <chris.j.rama@gmail.com>
2020-02-11 22:26:46 -0600

    Adds docker-compose setup

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Link to="/">Home</Link>
-          <Link to="/">otherpage</Link>
+          <Link to="/otherpage">otherpage</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
